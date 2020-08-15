11be5864b7b57f14a0bdb32c41643c90756be873
eidanjacob <69485423+eidanjacob@users.noreply.github.com>
2020-08-15 13:04:17 -0400

    drawing circles for player coin counts

--- SplendorFiles/gameInterface.py
+++ SplendorFiles/gameInterface.py
@@ -3,8 +3,15 @@ from pygame.locals import *
 
 
 LTBROWN = (75, 25, 0)
-GRAY = (128, 128, 128)
-BLACK = (0, 0, 0)
+gry = "Colors[gry]"
+blk = "Colors[blk]"
+wht = "white"
+blu = "blue"
+grn = "green"
+red = "red"
+gld = "gold"
+Colors = { blk : (0, 0, 0), wht : (255, 255, 255), blu : (0, 0, 255), grn : (0, 255, 0), red : (255, 0, 0), gld : (255, 255, 0), gry : (128, 128, 128) }
+colorNames = [blk, wht, blu, grn, red, gld]
 
 class GameInterface:
 
@@ -23,22 +30,38 @@ class GameInterface:
                             [int(x) for x in [w * 0.01, h * 0.11]], \
                             [int(x) for x in [w - h * 0.14, h * 0.11]]]
         self.playerScores = [[group[0], int(group[1] + h * 0.025)] for group in self.playerNames ]
-                             
+        self.playerCoins = [int(w * 0.29), int(h * 0.03), int(w * 0.29), int(h * 0.03) + 35, int(w*0.29), int(h * 0.03) + 70, \
+                            int(w * 0.29) + 35, int(h * 0.03), int(w * 0.29) + 35, int(h * 0.03) + 35, int(w*0.29) + 35, int(h * 0.03) + 70, \
+                            # Player 2
+                            int(w * 0.29), int(h * 0.88), int(w * 0.29), int(h * 0.88) + 35, int(w*0.29), int(h * 0.88) + 70, \
+                            int(w * 0.29) + 35, int(h * 0.88), int(w * 0.29) + 35, int(h * 0.88) + 35, int(w*0.29) + 35, int(h * 0.88) + 70, \
+                            # Player 3
+                            int(w * 0.02), int(h * 0.18), int(w * 0.02) + 35, int(h * 0.18), int(w*0.02)+70, int(h * 0.18), \
+                            int(w * 0.02), int(h * 0.18) + 35, int(w * 0.02) + 35, int(h * 0.18) + 35, int(w*0.02) + 70, int(h * 0.18) + 35, \
+                            # Player 4
+                            int(w * 0.91), int(h * 0.18), int(w * 0.91) + 35, int(h * 0.18), int(w*0.91)+70, int(h * 0.18), \
+                            int(w * 0.91), int(h * 0.18) + 35, int(w * 0.91) + 35, int(h * 0.18) + 35, int(w*0.91) + 70, int(h * 0.18) + 35]
 
     def Draw(self):
         self.screen.fill(LTBROWN)
         for i in range(len(self.gameController.Players)):
             # Draw Player Tableaus
-            pygame.draw.rect(self.screen, GRAY, self.tableaus[i])
+            pygame.draw.rect(self.screen, Colors[gry], self.tableaus[i])
             thisPlayer = self.gameController.Players[i]
-            label = self.font.render(thisPlayer.Name, True, BLACK, GRAY)
+            label = self.font.render(thisPlayer.Name, True, Colors[blk], Colors[gry])
             self.screen.blit(label, label.get_rect(left = self.playerNames[i][0], top = self.playerNames[i][1]))
-            score = self.font.render("Score: " + str(thisPlayer.Score), True, BLACK, GRAY)
+            score = self.font.render("Score: " + str(thisPlayer.Score), True, Colors[blk], Colors[gry])
             self.screen.blit(score, score.get_rect(left = self.playerScores[i][0], top = self.playerScores[i][1]))
         
         # draw card stacks
         
         # draw coins
+        for i in range(len(self.gameController.Players)):
+            for j in range(6):
+                col = Colors[colorNames[j]]
+                left = self.playerCoins[12*i + 2*j]
+                top = self.playerCoins[12*i + 2*j+1]
+                pygame.draw.circle(self.screen, col, (left, top), 15)
         
         for i in range(4):
             # Draw cards on table
